5ca296b7f8ac13338a137ef8fcdda80998f1a3c5
Iqbal Novramadani <iqbal.slf@gmail.com>
2023-08-24 18:05:40 +0700

    feat: add papertrail logging

--- src/lib/logger.ts
+++ src/lib/logger.ts
@@ -1,21 +1,43 @@
 import * as cluster from "cluster";
+import * as os from "os";
 
 import { Logger, LoggerInstance, transports } from "winston";
+import * as winstonPapertrail from "winston-papertrail";
 
 class Lgger {
   private logger: LoggerInstance;
 
   constructor() {
-    this.logger = new Logger({
-      transports: [
-        new transports.Console({
-          colorize: true,
-          handleExceptions: true,
-          humanReadableUnhandledException: true,
-          timestamp: true
-        })
-      ]
-    });
+    if (process.env.PAPERTRAIL_HOST && process.env.PAPERTRAIL_PORT) {
+      this.logger = new Logger({
+        transports: [
+          new transports.Console({
+            colorize: true,
+            handleExceptions: true,
+            humanReadableUnhandledException: true,
+            timestamp: true
+          }),
+          new winstonPapertrail.Papertrail({
+            handleExceptions: true,
+            host: process.env.PAPERTRAIL_HOST,
+            hostname: process.env.ROUTER_HOSTNAME || os.hostname(),
+            humanReadableUnhandledException: true,
+            port: process.env.PAPERTRAIL_PORT
+          })
+        ]
+      });
+    } else {
+      this.logger = new Logger({
+        transports: [
+          new transports.Console({
+            colorize: true,
+            handleExceptions: true,
+            humanReadableUnhandledException: true,
+            timestamp: true
+          })
+        ]
+      });
+    }
   }
 
   public info = (msg: string, ...meta: any[]): void => {
